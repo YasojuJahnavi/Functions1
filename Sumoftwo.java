ba61237f1ff40920ec7c1bb9906b93f52a208c0e
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 13:07:36 +0530

    Add files via upload

---
+++ Sumoftwo.java
@@ -0,0 +1,12 @@
+public class Sumoftwo{
+   
+    public static int sum(int num1,int num2){
+        return num1+num2;
+    }
+    public static void main(String[]args){
+        int number1=8;
+        int number2=20;
+        int result=sum(number1,number2);
+        System.out.println("the sum of"+number1+"and "+number2+"is:"+result);
+}
+}
